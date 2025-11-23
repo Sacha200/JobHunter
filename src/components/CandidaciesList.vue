530c54ebf884b0792486f1f1dd86d905a747c4ec
Sacha200 <nahmani.sacha@orange.fr>
2025-11-23 20:32:19 +0100

    refactor(components): ajustements des composants de liste

--- src/components/CandidaciesList.vue
+++ src/components/CandidaciesList.vue
@@ -1,55 +1,36 @@
 <script setup>
-
-
-
-
-import router from "@/router"
-
-
-
-async function updateCandidacy(id) {
-router.push(`/candidacy-single/${id}`)
-
-}
-
-
+import router from "@/router";
 
 const props = defineProps({
+  candidacy: {
+    type: Object,
+  },
+});
 
-    candidacy: {
-        type: Object,
-    }
-}); 
-
-
-
-
+async function updateCandidacy(id) {
+  router.push(`/candidacy-single/${id}`);
+}
 </script>
 
-<style scoped></style>
-
 <template>
-    <!-- <pre>{{ candidacy }}</pre>  -->
+  <article class="card candidacy-card">
+    <img
+      :src="
+        candidacy.image && candidacy.image[0]
+          ? candidacy.image[0].signedUrl
+          : '/src/assets/img/image1.jpg'
+      "
+      class="card-img-top img-fluid"
+      alt=""
+    />
+    <div class="card-body">
+      <p class="card-meta">{{ candidacy.location }} • {{ candidacy.date }}</p>
+      <h5 class="card-title fw-bold">{{ candidacy.entreprise }}</h5>
+      <p class="card-text">{{ candidacy.title }}</p>
 
-
-    <div class="card" style="width: 20rem;">
-        <img :src="candidacy.image[0].signedUrl" class="card-img-top img-fluid">
-        <div class="card-body">
-            <h5 class="card-title fw-bold">{{ candidacy.entreprise }}</h5>
-            <p class="card-text">{{ candidacy.title }}</p>
-            <p class="card-text">{{ candidacy.location }}</p>
-            <p class="card-text">{{ candidacy.date }}</p>
-
-            <button class="fw-semibold btn" id="candidacies"
-                @click="updateCandidacy(candidacy.Id)">voir infos</button>
-        </div>
+      <button class="btn btn-primary fw-semibold" @click="updateCandidacy(candidacy.Id)">
+        Voir infos
+      </button>
     </div>
-
-
-
-
-
-
-
-
+  </article>
 </template>
--- src/components/WebsitesList.vue
+++ src/components/WebsitesList.vue
@@ -18,24 +18,17 @@ const props = defineProps({
 <style scoped></style>
 
 <template>
-    <!-- <pre>{{ site }}</pre> -->
-
-
-    <div class="card mb-5 siteslist" style="max-width: 800px; height: 200px;">
-      <div class="row g-2 text-center   ">
-        <div class="col-md-3">
-            <RouterLink :to="`/website/${site.id_websites}`">
-                <img  :src="site.image[0].signedUrl" class="img-fluid  p-4" id="site_logo" />
-            </RouterLink>  
-        </div>      
-        <div class="col-md-9">
-          <div class="card-body text-start">
-            <h5 class="card-title">{{ site.title }}</h5>
-            <p class="card-text">{{ site.description }}</p>
-            <p><a class="link-offset-2 link-underline link-underline-opacity-100" href="#"> voir site</a></p>          </div>
-        </div>
+    <article class="card website-card mb-4 siteslist">
+      <div class="site-logo">
+        <RouterLink :to="`/website/${site.id_websites}`">
+          <img :src="site.image[0].signedUrl" class="img-fluid" alt="" />
+        </RouterLink>
       </div>
-        </div>
 
-    
+      <div class="card-body text-start">
+        <h5 class="card-title">{{ site.title }}</h5>
+        <p class="card-text">{{ site.description }}</p>
+        <a class="card-link" :href="site.url || '#'" target="_blank" rel="noopener">voir site</a>
+      </div>
+    </article>
 </template>
